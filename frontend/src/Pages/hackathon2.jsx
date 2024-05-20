b5d99dd1e9c2a2b32970587f8780cf27bf08270b
Ashish Kumar Samantaray <121350516+AshishKumarSamantaray@users.noreply.github.com>
2024-05-20 19:25:08 +0530

    changes done

---
+++ frontend/src/Pages/hackathon2.jsx
@@ -0,0 +1,133 @@
+import "./hackcss.css";
+
+const Hack2 = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="hackathontitle"><b style={{ fontWeight: 700 }}>Hack</b>athon</h1>
+                <h5 className="taglinesec">Where passion meets coding</h5>
+                <br />
+                <br />
+                <br />
+                <div className="cardscontain">
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <br />
+                <div className="cardscontain">
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <br />
+                <div className="cardscontain">
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="ag-courses_item">
+                        <a href="https://example.com" className="ag-courses-item_link">
+                            <div className="ag-courses-item_bg" />
+                            <div className="ag-courses-item_title">
+                                UI/Web&amp;Graph design for teenagers 11-17&nbsp;years old
+                            </div>
+                            <div className="ag-courses-item_date-box">
+                                Start:
+                                <span className="ag-courses-item_date">04.11.2022</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hack2;
